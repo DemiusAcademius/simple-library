dbbd578a6435600136430155a764f19b2b1ec86e
Dmitri Astafiev <DemiusAcademius@users.noreply.github.com>
2022-02-24 20:18:46 +0200

    add same books to database

--- src/main/resources/databasse/Populate library.sql
+++ src/main/resources/databasse/Populate library.sql
@@ -18,12 +18,27 @@ VALUES
 
 INSERT INTO authors (first_name,last_name) VALUES ('Кип','Торн');
 INSERT INTO authors (first_name,last_name) VALUES ('Карло','Ровелли');
+INSERT INTO authors (first_name,last_name) VALUES ('Аркадий и Борис','Стругацкие');
+INSERT INTO authors (first_name,last_name) VALUES ('Айн','Рэнд');
+INSERT INTO authors (first_name,last_name) VALUES ('Роберт','Кийосаки');
+INSERT INTO authors (first_name,last_name) VALUES ('Филип','Зимбардо');
 
 INSERT INTO publishers (name) VALUES ('Манн, Иванов и Фербер');
 INSERT INTO publishers (name) VALUES ('Corpus');
+INSERT INTO publishers (name) VALUES ('Ардис');
+INSERT INTO publishers (name) VALUES ('Альпина Диджитал');
+INSERT INTO publishers (name) VALUES ('Попурри');
 
 INSERT INTO books VALUES ('9785000575369','Интерстеллар: наука за кадром', 1, 2014, 'https://www.litres.ru/kip-torn/interstellar-nauka-za-kadrom/chitat-onlayn/');
-INSERT INTO books VALUES ('9785171149857','Интерстеллар: наука за кадром', 2, 2017, 'https://www.litres.ru/karlo-rovelli/srok-vremeni/chitat-onlayn/');
+INSERT INTO books VALUES ('9785171149857','Срок времени', 2, 2017, 'https://www.litres.ru/karlo-rovelli/srok-vremeni/chitat-onlayn/');
+INSERT INTO books VALUES ('4607031764114','Понедельник начинается в субботу', 3, 2017, 'https://cv8.litres.ru/pub/c/audiokniga/cover_415/3956285-arkadiy-i-boris-strugackie-ponedelnik-nachinaetsya-v-subbotu-3956285.jpg');
+INSERT INTO books VALUES ('9785961420043','Атлант расправил плечи', 4, 2012, 'https://cv7.litres.ru/pub/c/elektronnaya-kniga/cover_415/4236675-ayn-rend-atlant-raspravil-plechi.jpg');
+INSERT INTO books VALUES ('9789851523241','Богатый папа, бедный папа', 5, 2015, 'https://cv5.litres.ru/pub/c/elektronnaya-kniga/cover_415/119256-robert-kiyosaki-bogatyy-papa-bednyy-papa.jpg');
+INSERT INTO books VALUES ('9785001396864','Эффект Люцифера. Почему хорошие люди превращаются в злодеев', 4, 2022, 'https://cv3.litres.ru/pub/c/audiokniga/cover_415/67229830-filip-zimbardo-effekt-lucifera-pochemu-horoshie-ludi-prevraschaut-67229830.jpg');
 
 INSERT INTO book_authors VALUES ('9785000575369', 1);
 INSERT INTO book_authors VALUES ('9785171149857', 2);
+INSERT INTO book_authors VALUES ('4607031764114', 3);
+INSERT INTO book_authors VALUES ('9785961420043', 4);
+INSERT INTO book_authors VALUES ('9789851523241', 5);
+INSERT INTO book_authors VALUES ('9785001396864', 6);
